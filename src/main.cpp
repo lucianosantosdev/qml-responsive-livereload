c822a0963a9a0bff99c230532ae48fed63cef352
luciano santos <luciano.santos@aditum.com.br>
2020-07-03 15:38:26 -0300

    Remove some includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-// main.cpp
-#include <QHBoxLayout>
-#include <QtQml/QQmlApplicationEngine>
-#include <QtQuick/QQuickWindow>
-#include <QtWidgets/QApplication>
-#include <QtWidgets>
+#include <QApplication>
 
 #include "mainwindow.h"
+
 int
 main(int argc, char* argv[])
 {
   qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-
   if (qEnvironmentVariableIsEmpty("QTGLESSTREAM_DISPLAY")) {
     qputenv("QT_QPA_EGLFS_PHYSICAL_WIDTH", QByteArray("213"));
     qputenv("QT_QPA_EGLFS_PHYSICAL_HEIGHT", QByteArray("120"));
